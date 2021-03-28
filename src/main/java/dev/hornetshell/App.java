edcbfd2503829a8c3c07a6c30718cb07a8d720fa
Devin <devin@localhost.localdomain>
2021-03-28 16:01:37 -0400

    update to use sphere coordinates

--- src/main/java/dev/hornetshell/App.java
+++ src/main/java/dev/hornetshell/App.java
@@ -1,5 +1,6 @@
 package dev.hornetshell;
 
+import java.util.Random;
 import java.util.Timer;
 
 import dev.hornetshell.tasks.CollisionTask;
@@ -10,6 +11,10 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
@@ -32,21 +37,33 @@ public class App extends Application {
 		final PerspectiveCamera camera = new PerspectiveCamera();
 		camera.setNearClip(0.1);
 		camera.setFarClip(1000.0);
-		camera.setTranslateZ(-450);
-		camera.setRotationAxis(Rotate.X_AXIS);
-		camera.setRotate(33.0);
-		
-		
+		camera.setTranslateZ(0);
+		camera.setTranslateX(320);
+		camera.setTranslateY(240);
+			
 		return camera;
+	}
+	
+	private void makeRandomSpheres() {
+		final Random random = new Random(0);
+		for(int i=0; i < 10; i++) {
+			final var sphere = new Group();
+	        sphere.setTranslateX(random.nextInt(640));
+	        sphere.setTranslateY(random.nextInt(480));
+	        sphere.setTranslateZ(random.nextInt(500));
+	        final var sphObj = new Sphere();
+	        sphObj.setRadius(random.nextInt(50));
+	        final var material = new PhongMaterial();
+	        material.setDiffuseColor(Color.ALICEBLUE);
+	        material.setSpecularColor(Color.DARKBLUE);
+	        sphObj.setMaterial(material);
+	        sphere.getChildren().add(sphObj);
+	        world.getChildren().add(sphere);
+		}
 	}
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-        
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        world.getChildren().add(new StackPane(label));
         var scene = new Scene(world, 640, 480);
         final Rotate rx = new Rotate(0, Rotate.X_AXIS);
         final Rotate ry = new Rotate(0, Rotate.Y_AXIS);
@@ -54,6 +71,9 @@ public class App extends Application {
         camera.getTransforms().addAll(rx, ry);
         scene.setCamera(camera);
         stage.setScene(scene);
+        
+        makeRandomSpheres();
+        
         KeyboardHandler.handleKeyboard(scene, world, rx, ry);
         stage.show();
     }
--- src/main/java/dev/hornetshell/KeyboardHandler.java
+++ src/main/java/dev/hornetshell/KeyboardHandler.java
@@ -21,35 +21,89 @@ public class KeyboardHandler {
 
 			@Override
 			public void handle(KeyEvent event) {
+				final double x = camera.getTranslateX();
+				final double y = camera.getTranslateY();
+				final double z = camera.getTranslateZ();
+				final double ya = ry.getAngle();
+				final double xa = rx.getAngle();
+				double[] sph;
+				double[] p1;
 				switch (event.getCode()) {
 				case UP: 
-					rx.setAngle(rx.getAngle()+SPEED);
+					System.out.println("xa: "+xa);
+					rx.setAngle(xa+SPEED);
 					break;
-				case DOWN: 
-					rx.setAngle(rx.getAngle()-SPEED);
+				case DOWN:
+					System.out.println("xa: "+xa);
+					rx.setAngle(xa-SPEED);
 					break;
 				case LEFT: 
-					ry.setAngle(ry.getAngle()-SPEED);
+					System.out.println("ya: "+ya);
+					ry.setAngle(ya-SPEED);
 					break;
 				case RIGHT: 
-					ry.setAngle(ry.getAngle()+SPEED);
+					System.out.println("ya: "+ya);
+					ry.setAngle(ya+SPEED);
 					break;
 				case W:
-					camera.setTranslateZ(camera.getTranslateZ()+SPEED);
+					sph = cartToSphere(new double[] {x, y, z});
+					sph[0]-=SPEED;
+					p1 = sphereToCart(sph);
+					camera.setTranslateX(p1[0]);
+					camera.setTranslateY(p1[1]);
+					camera.setTranslateZ(p1[2]);
 					break;
 				case S:
-					camera.setTranslateZ(camera.getTranslateZ()-SPEED);
+					sph = cartToSphere(new double[] {x, y, z});
+					sph[0]+=SPEED;
+					p1 = sphereToCart(sph);
+					camera.setTranslateX(p1[0]);
+					camera.setTranslateY(p1[1]);
+					camera.setTranslateZ(p1[2]);
 					break;
 				case A:
-					camera.setTranslateX(camera.getTranslateX()-SPEED);
+					// turn left
+					sph = cartToSphere(new double[] {x, y, z});
+					sph[1]+=Math.PI/10d;
+					p1 = sphereToCart(sph);
+					camera.setTranslateX(p1[0]);
+					camera.setTranslateY(p1[1]);
+					camera.setTranslateZ(p1[2]);
 					break;
 				case D:
-					camera.setTranslateX(camera.getTranslateX()+SPEED);
+					// turn right
+					sph = cartToSphere(new double[] {x, y, z});
+					sph[1]-=Math.PI/10d;
+					p1 = sphereToCart(sph);
+					camera.setTranslateX(p1[0]);
+					camera.setTranslateY(p1[1]);
+					camera.setTranslateZ(p1[2]);
+					break;
+				case Z:
+					rx.setAngle(0);
+					ry.setAngle(0);
 					break;
 				default: break;
 				}
 			}
 		});
 	}
+	
+	private static double[] cartToSphere(double[] xyz) {
+		final double p = Math.sqrt(Math.pow(xyz[0], 2) + Math.pow(xyz[1], 2) + Math.pow(xyz[2], 2));
+		final double theta = Math.atan2(xyz[1], xyz[0]);
+		final double phi = Math.acos(xyz[2]/p);
+		System.out.println("Sphere: "+p+", "+theta+", "+phi);
+		return new double[] {p, theta!=theta?0:theta, phi!=phi?0:phi};
+	}
+	
+	private static double[] sphereToCart(double[] ptp) {
+		final double x = ptp[0] * Math.sin(ptp[2]) * Math.cos(ptp[1]);
+		final double y = ptp[0] * Math.sin(ptp[2]) * Math.sin(ptp[1]);
+		final double z = ptp[0] * Math.cos(ptp[2]);
+		System.out.println("Cart:"+x+", "+y+", "+z);
+		return new double[] {x, y, z};
+	}
+	
 
 }
